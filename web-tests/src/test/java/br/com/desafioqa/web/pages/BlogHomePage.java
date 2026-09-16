a3756514ef906531a695009f52e272fa3c4b1066
Daniel Barbosa <danielbarbosaj456@gmail.com>
2026-09-16 19:24:27 -0300

    fix: atualiza busca do Blog do Agi

--- web-tests/src/test/java/br/com/desafioqa/web/pages/BlogHomePage.java
+++ web-tests/src/test/java/br/com/desafioqa/web/pages/BlogHomePage.java
@@ -2,7 +2,9 @@ package br.com.desafioqa.web.pages;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,13 +16,17 @@ import java.util.List;
 public class BlogHomePage {
 
     private static final String DEFAULT_URL = "https://blogdoagi.com.br/";
+    private static final Duration SEARCH_OVERLAY_TIMEOUT = Duration.ofSeconds(6);
 
     private final WebDriver driver;
     private final WebDriverWait wait;
 
     private final By searchToggles = By.cssSelector(
-            ".ast-header-search .astra-search-icon, .ast-search-menu-icon .search-icon");
-    private final By searchFields = By.cssSelector("form.search-form input[name='s']");
+            "a.full-screen.astra-search-icon[role='button'], "
+                    + ".ast-header-search .astra-search-icon, "
+                    + ".ast-search-menu-icon .search-icon");
+    private final By searchFields = By.cssSelector(
+            "form.search-form input.search-field[name='s']");
 
     public BlogHomePage(WebDriver driver) {
         this.driver = driver;
@@ -38,14 +44,30 @@ public class BlogHomePage {
     public SearchResultsPage searchFor(String term) {
         WebElement field = firstDisplayed(searchFields);
         if (field == null) {
-            WebElement toggle = wait.until(driver -> firstDisplayed(searchToggles));
+            WebElement toggle = wait.until(ExpectedConditions.elementToBeClickable(searchToggles));
             toggle.click();
-            field = wait.until(driver -> firstDisplayed(searchFields));
+
+            try {
+                field = new WebDriverWait(driver, SEARCH_OVERLAY_TIMEOUT)
+                        .until(currentDriver -> firstDisplayed(searchFields));
+            } catch (TimeoutException overlayDidNotOpen) {
+                submitExistingSearchForm(term);
+                return new SearchResultsPage(driver).waitUntilLoaded(term);
+            }
         }
 
         field.clear();
         field.sendKeys(term, Keys.ENTER);
         return new SearchResultsPage(driver).waitUntilLoaded(term);
+    }
+
+    private void submitExistingSearchForm(String term) {
+        WebElement field = wait.until(ExpectedConditions.presenceOfElementLocated(searchFields));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].value = arguments[1]; arguments[0].form.submit();",
+                field,
+                term
+        );
     }
 
     private WebElement firstDisplayed(By locator) {
